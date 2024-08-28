e77d7dbfb5abb282bdc657e6590c733e96f62479
Aurelius Ivan Wijaya <aureliusivanwijaya@gmail.com>
2024-08-28 22:54:51 +0700

    feat/add-outstatic

--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -1,4 +1,4 @@
-export default function RootLayout({ children }) {
+export default function RootLayout({children}: { children: any }) {
   return (
       <html lang="en">
       <body>{children}</body>
