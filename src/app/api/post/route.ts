58d62b4c9df64916145c12167996314e6de33300
ivan <aureliusivanwijaya@gmail.com>
2025-02-09 02:50:37 +0700

    fix: remove commented-out code in post API route

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -3,6 +3,5 @@ import { getDocuments } from "outstatic/server";
 export const dynamic = 'force-static';
 
 export const GET = async () => {
-    // return res.json(result)
     return getDocuments('posts', ['title', "description", "slug"])
 }
